46d53828aa55a3b10af038eab0637a3a670e5352
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2023-04-28 07:23:17 +0300

    Simplified mission

--- mission.py
+++ mission.py
@@ -22,17 +22,18 @@ def main(connection_string):
     print(vehicle.mode.__str__())
     print('Arm/Armable: %s/%s' % (vehicle.armed, vehicle.is_armable))
 
-    print("##### GETTING THE WAYPOINTS #####")
-    cmds = vehicle.commands
+    # print("##### GETTING THE WAYPOINTS #####")
+    # cmds = vehicle.commands
 
-    cmds.download()
-    cmds.wait_ready()
+    # cmds.download()
+    # cmds.wait_ready()
 
-    for cmd in cmds:
-        wps.append(cmd)
+    # for cmd in cmds:
+    #     wps.append(cmd)
 
     print('##### TARGET DETECTION STARTED #####')
-    pos_map = []
+    frame_map = {}
+    pos_list = []
     target_gps_pos = None
 
     vid = cv2.VideoCapture(0)
@@ -42,40 +43,44 @@ def main(connection_string):
         frame, vehicle_params = utils.detect(frame=frame, vehicle=vehicle)
 
         if vehicle_params is not None:
-            pos_map.append(vehicle_params)
+            frame_map[f'{time.time()}_{vehicle_params}.jpg'] = frame
+            pos_list.append(vehicle_params)
 
-        if len(pos_map) > DET_THRESH_FRAME_NUM:
-            target_gps_pos = utils.target_finder(pos_map)
+        if len(pos_list) > DET_THRESH_FRAME_NUM:
+            target_gps_pos = utils.target_finder(pos_list)
             break
 
     vid.release()
 
     print('##### TARGET DETECTED #####')
     print('Target position: ', target_gps_pos)
+    
+    for filename, frame in frame_map:
+        cv2.imwrite(filename, frame)
 
-    print('##### SETTING NEW WAYPOINTS #####')
-    wps[WP_IDX_TARGET_1] = Command(
-        0, 0, 0,
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
-        target_gps_pos[0], target_gps_pos[1], WP_TARGET_ALT
-    )
+    # print('##### SETTING NEW WAYPOINTS #####')
+    # wps[WP_IDX_TARGET_1] = Command(
+    #     0, 0, 0,
+    #     mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+    #     mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
+    #     target_gps_pos[0], target_gps_pos[1], WP_TARGET_ALT
+    # )
 
-    wps[WP_IDX_TARGET_2] = Command(
-        0, 0, 0,
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
-        target_gps_pos[0], target_gps_pos[1], WP_TARGET_ALT
-    )
+    # wps[WP_IDX_TARGET_2] = Command(
+    #     0, 0, 0,
+    #     mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+    #     mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0,
+    #     target_gps_pos[0], target_gps_pos[1], WP_TARGET_ALT
+    # )
 
-    print('##### UPLOADING NEW WAYPOINTS #####')
-    cmds.clear()
+    # print('##### UPLOADING NEW WAYPOINTS #####')
+    # cmds.clear()
 
-    for wp in wps:
-        cmds.add(wp)
+    # for wp in wps:
+    #     cmds.add(wp)
 
-    cmds.upload()
-    print('##### NEW WAYPOINTS ARE UPLOADED #####')
+    # cmds.upload()
+    # print('##### NEW WAYPOINTS ARE UPLOADED #####')
     
     while True:
         time.sleep(3)
